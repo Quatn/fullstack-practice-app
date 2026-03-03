3caf67e993b389daf4b73bd1394b2827ec583b35
Quatn <nguyenminhquanvh@gmail.com>
2026-03-03 16:56:17 +0700

    init(be): Setup jwt service

--- BE/src/main/java/com/example/demo_springboot_api/config/SecurityConfig.java
+++ BE/src/main/java/com/example/demo_springboot_api/config/SecurityConfig.java
@@ -12,10 +12,13 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
-        .csrf(csrf -> csrf.disable()) // disable for API usage
+        .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/").permitAll()
+            .requestMatchers("/test").permitAll()
+            .requestMatchers("/test2").permitAll()
             .anyRequest().authenticated());
+        // .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
     return http.build();
   }
