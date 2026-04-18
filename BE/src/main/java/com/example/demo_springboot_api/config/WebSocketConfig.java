8a13c9062d8aaf8378942f88db72ff59625fe645
Quatn <nguyenminhquanvh@gmail.com>
2026-04-18 10:44:05 +0700

    feat(api): Add dependency and config for org.springframework.web.socket

---
+++ BE/src/main/java/com/example/demo_springboot_api/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo_springboot_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    // Domain for messages from the server to the client
+    config.enableSimpleBroker("/topic");
+
+    // "Domain" for messages from the client to the server
+    config.setApplicationDestinationPrefixes("/app");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/chatapp").withSockJS();
+  }
+}
